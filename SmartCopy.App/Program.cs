909bbccc59a363dee916e03167570d67d46b4f7b
machinewrapped <machinewrapped@users.noreply.github.com>
2026-02-20 20:23:32 +0100

    UI mockup

--- SmartCopy.App/Program.cs
+++ SmartCopy.App/Program.cs
@@ -8,9 +8,15 @@ class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Available resources in UI Assembly:");
+        foreach (var res in typeof(SmartCopy.UI.Views.MainWindow).Assembly.GetManifestResourceNames())
+        {
+            Console.WriteLine("  " + res);
+        }
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
